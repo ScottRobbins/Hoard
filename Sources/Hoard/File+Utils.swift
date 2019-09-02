07dc8187956548a37c96eebc30e2b8cc96ee9fd8
Scott Robbins <scott.robbins@mobiledefense.com>
2019-09-01 20:59:39 -0400

    Add ability to copy to a specific path

--- Sources/Hoard/File+Utils.swift
+++ Sources/Hoard/File+Utils.swift
@@ -11,6 +11,19 @@ extension File {
         return try copy(to: folder)
     }
 
+    func copy(toPath newPath: String, overwrite: Bool) throws -> File {
+        let fileManager = FileManager.default
+        let absoluteNewPath = try fileManager.absolutePath(for: newPath)
+        let absolutePath = try fileManager.absolutePath(for: path)
+
+        if overwrite, let deleteFile = try? File(path: newPath) {
+            try deleteFile.delete()
+        }
+
+        try fileManager.copyItem(atPath: absolutePath, toPath: absoluteNewPath)
+        return try File(path: absoluteNewPath)
+    }
+
     func rename(to newName: String, overwrite: Bool) throws {
         if overwrite {
             if let parent = parent, let file = try? parent.file(named: newName) {
@@ -21,5 +34,61 @@ extension File {
         }
 
         try rename(to: newName, keepExtension: false)
+    }
+}
+
+// Credit to https://github.com/JohnSundell/Files for this part
+extension FileManager {
+    func absolutePath(for path: String) throws -> String {
+        if path.hasPrefix("/") {
+            return try pathByFillingInParentReferences(for: path)
+        }
+
+        if path.hasPrefix("~") {
+            let prefixEndIndex = path.index(after: path.startIndex)
+
+            let path = path.replacingCharacters(
+                in: path.startIndex..<prefixEndIndex,
+                with: ProcessInfo.processInfo.homeFolderPath
+            )
+
+            return try pathByFillingInParentReferences(for: path)
+        }
+
+        return try pathByFillingInParentReferences(for: path, prependCurrentFolderPath: true)
+    }
+
+    func pathByFillingInParentReferences(for path: String, prependCurrentFolderPath: Bool = false) throws -> String {
+        var path = path
+        var filledIn = false
+
+        while let parentReferenceRange = path.range(of: "../") {
+            let currentFolderPath = String(path[..<parentReferenceRange.lowerBound])
+
+            guard let currentFolder = try? Folder(path: currentFolderPath) else {
+                throw FileSystem.Item.PathError.invalid(path)
+            }
+
+            guard let parent = currentFolder.parent else {
+                throw FileSystem.Item.PathError.invalid(path)
+            }
+
+            path = path.replacingCharacters(in: path.startIndex..<parentReferenceRange.upperBound, with: parent.path)
+            filledIn = true
+        }
+
+        if prependCurrentFolderPath {
+            guard filledIn else {
+                return currentDirectoryPath + "/" + path
+            }
+        }
+
+        return path
+    }
+}
+
+extension ProcessInfo {
+    var homeFolderPath: String {
+        return environment["HOME"]!
     }
 }
